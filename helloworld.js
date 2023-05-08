0749eace8526e5241ac76595fef20bc726a6b192
Husain Unwalla <mr.husainunwalla@gmail.com>
2023-05-08 15:40:29 -0700

    feat: code to insert an html element to the page

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,13 @@
-document.querySelector('#contents').remove();
+// Remove the feed
+document.querySelector('ytd-browse[role="main"][page-subtype="home"] #contents').remove();
+
+// Create a new <p> element
+var message = document.createElement("p");
+message.textContent = "Feed Hidden";
+message.style = "font-size: 50px; color: red; text-align: center;";
+
+// Get the element with id "primary"
+var primaryElement = document.getElementById("primary");
+
+// Append the <p> element to the "primary" element
+primaryElement.appendChild(message);
